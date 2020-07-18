6ce78e0eec84d32c0590cd0a6ef7e270a454c2cd
Cyle Ven Dawson <dawsonc623@gmail.com>
2020-07-18 10:06:37 -0500

    feat: implement Hello World Phaser app

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,1 +1,41 @@
-console.log("Hello, World!");
+import {
+  Game,
+  Scene
+} from "phaser";
+
+import "./index.scss";
+
+class HelloWorldScene extends Scene
+{
+  public constructor()
+  {
+    super("helloWorld");
+  }
+
+  public create()
+  {
+    const helloMessage = this.add.text(this.scale.width / 2, 100, "Hello, World!");
+    helloMessage.setOrigin(0.5);
+  }
+}
+
+new Game({
+  "type"  : Phaser.AUTO,
+
+  "physics" : {
+    "default" : "arcade"
+  },
+
+  "render"  : {
+    "pixelArt"    : true,
+    "roundPixels" : true
+  },
+
+  "scale" : {
+    "mode"  : Phaser.Scale.RESIZE
+  },
+
+  "scene" : [
+    HelloWorldScene
+  ]
+});
